6786c65b4f836e27212ad42d9af263da6c05e5bc
Avatar <prachaya.su@gmail.com>
2018-04-01 20:45:12 +0700

    edit config

--- config/env/production.js
+++ config/env/production.js
@@ -2,8 +2,9 @@
 
 module.exports = {
   port: process.env.PORT || 8443,
+  sessionSecret: process.env.SESSION_SECRET,
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean' || process.env.MONGODB_URI,
+    uri: process.env.MONGODB_URI,
     options: {
       user: '',
       pass: ''
